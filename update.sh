2aa22f25f2856853c86418ba32899353a1a2bf19
Javier <fjbatresv@gmail.com>
2020-04-28 19:13:35 -0600

    Changes for use bash script on generation

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Setup git
 git config user.name \"$1\"
 git config user.email \"$2\"
+# Update repo
 git pull
 git status
-git add *
-git commit -m "Updated using the bash file";
+# Create file with extensions
+code --list-extensions | xargs -L 1 echo code --install-extension
+# Update changes
+git add vscode_extensions.sh
+git commit -m "Updated using the bash file"
+git push
